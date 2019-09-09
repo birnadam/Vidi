72532cf555fdc7792488ca881d2fc1f66884c563
birna <briandam26@yahoo.com>
2019-09-09 11:56:32 -0700

    Changed profile page to for MVP

--- client/src/containers/Profile/index.js
+++ client/src/containers/Profile/index.js
@@ -12,41 +12,41 @@ const Profile = props => (
             </div>
             <div className="profile-body scroll-hijack">
                 <div className="row">
-                    <div className="col-lg-8 col-lg-offset-2">
+                    {/* <div className="col-lg-8 col-lg-offset-2"> */}
                         <div className="row valign">
                             <div className="col-md-6">
                                 <h6 className="text-bold mb-25">Friends List</h6>
                                 <div className="groups-list scroll-hijack">
                                     <ul className="list">
-                                        <li><a href="/"><div className="avatar"><img className="little_avatar" src="https://cdn1.iconfinder.com/data/icons/ninja-things-1/1772/ninja-simple-512.png" alt="" /></div>Binyan</a></li>
-                                        <li><a href="/"><div className="avatar"><img className="little_avatar" src="https://cdn1.iconfinder.com/data/icons/ninja-things-1/1772/ninja-simple-512.png" alt="" /></div>Coral</a></li>
-                                        <li><a href="/"><div className="avatar"><img className="little_avatar" src="https://cdn1.iconfinder.com/data/icons/ninja-things-1/1772/ninja-simple-512.png" alt="" /></div>Musa</a></li>
+                                        <li><a href="/profile"><div className="avatar"><img className="little_avatar" src="https://cdn1.iconfinder.com/data/icons/ninja-things-1/1772/ninja-simple-512.png" alt="" /></div>Binyan</a></li>
+                                        <li><a href="/profile"><div className="avatar"><img className="little_avatar" src="https://cdn1.iconfinder.com/data/icons/ninja-things-1/1772/ninja-simple-512.png" alt="" /></div>Coral</a></li>
+                                        <li><a href="/profile"><div className="avatar"><img className="little_avatar" src="https://cdn1.iconfinder.com/data/icons/ninja-things-1/1772/ninja-simple-512.png" alt="" /></div>Musa</a></li>
                                     </ul>
                                 </div>
                                 <h6 className="text-bold mt-25">Public Channels</h6>
                                 <p className="mb-25 clr-fade text-small">Click a group to copy the invite link to clipboard</p>
                                 <div className="groups-list scroll-hijack">
                                     <ul className="list">
-                                        <li><a href="/"><div className="avatar"><img className="little_avatar" src="https://cdn1.iconfinder.com/data/icons/ninja-things-1/1772/ninja-simple-512.png" alt="" /></div>Stranger Things</a></li>
-                                        <li><a href="/"><div className="avatar"><img className="little_avatar" src="https://cdn1.iconfinder.com/data/icons/ninja-things-1/1772/ninja-simple-512.png" alt="" /></div>Spiderman</a></li>
-                                        <li><a href="/"><div className="avatar"><img className="little_avatar" src="https://cdn1.iconfinder.com/data/icons/ninja-things-1/1772/ninja-simple-512.png" alt="" /></div>Parks and Rec</a></li>
+                                        <li><a href="/profile"><div className="avatar"><img className="little_avatar" src="https://cdn1.iconfinder.com/data/icons/ninja-things-1/1772/ninja-simple-512.png" alt="" /></div>Stranger Things</a></li>
+                                        <li><a href="/profile"><div className="avatar"><img className="little_avatar" src="https://cdn1.iconfinder.com/data/icons/ninja-things-1/1772/ninja-simple-512.png" alt="" /></div>Spiderman</a></li>
+                                        <li><a href="/profile"><div className="avatar"><img className="little_avatar" src="https://cdn1.iconfinder.com/data/icons/ninja-things-1/1772/ninja-simple-512.png" alt="" /></div>Parks and Rec</a></li>
                                     </ul>
                                 </div>
                                 <h6 className="text-bold mt-25">Private Channels</h6>
                                 <p className="mb-25 clr-fade text-small">Click a group to copy the invite link to clipboard</p>
                                 <div className="groups-list scroll-hijack">
                                     <ul className="list">
-                                        <li><a href="/"><div className="avatar"><img className="little_avatar" src="https://cdn1.iconfinder.com/data/icons/ninja-things-1/1772/ninja-simple-512.png" alt="" /></div>Stranger Things W/ Spoils</a></li>
-                                        <li><a href="/"><div className="avatar"><img className="little_avatar" src="https://cdn1.iconfinder.com/data/icons/ninja-things-1/1772/ninja-simple-512.png" alt="" /></div>My Hero Academia W/ Spoils</a></li>
-                                        <li><a href="/"><div className="avatar"><img className="little_avatar" src="https://cdn1.iconfinder.com/data/icons/ninja-things-1/1772/ninja-simple-512.png" alt="" /></div>Demon Slayer W/ Spoils</a></li>
+                                        <li><a href="/profile"><div className="avatar"><img className="little_avatar" src="https://cdn1.iconfinder.com/data/icons/ninja-things-1/1772/ninja-simple-512.png" alt="" /></div>Stranger Things W/ Spoils</a></li>
+                                        <li><a href="/profile"><div className="avatar"><img className="little_avatar" src="https://cdn1.iconfinder.com/data/icons/ninja-things-1/1772/ninja-simple-512.png" alt="" /></div>My Hero Academia W/ Spoils</a></li>
+                                        <li><a href="/profile"><div className="avatar"><img className="little_avatar" src="https://cdn1.iconfinder.com/data/icons/ninja-things-1/1772/ninja-simple-512.png" alt="" /></div>Demon Slayer W/ Spoils</a></li>
                                     </ul>
                                 </div>
                                 <h6 className="text-bold mt-25">Recent Ratings</h6>
                                 <div className="groups-list scroll-hijack">
                                     <ul className="list">
-                                        <li><a href="/"><div className="avatar"><img className="little_avatar" src="https://cdn1.iconfinder.com/data/icons/ninja-things-1/1772/ninja-simple-512.png" alt="" /></div>Demon Slayer&nbsp;  S.1 E.3 &nbsp;&nbsp;          Rating: 5</a></li>
-                                        <li><a href="/"><div className="avatar"><img className="little_avatar" src="https://cdn1.iconfinder.com/data/icons/ninja-things-1/1772/ninja-simple-512.png" alt="" /></div>Stranger Things&nbsp;  S.1E.8    &nbsp;&nbsp;       Rating: 5</a></li>
-                                        <li><a href="/"><div className="avatar"><img className="little_avatar" src="https://cdn1.iconfinder.com/data/icons/ninja-things-1/1772/ninja-simple-512.png" alt="" /></div>Game of Thrones    &nbsp;&nbsp;    Rating: 1</a></li>
+                                        <li><a href="/profile"><div className="avatar"><img className="little_avatar" src="https://cdn1.iconfinder.com/data/icons/ninja-things-1/1772/ninja-simple-512.png" alt="" /></div>Demon Slayer&nbsp;S.1 E.3&nbsp;Rating: 5</a></li>
+                                        <li><a href="/profile"><div className="avatar"><img className="little_avatar" src="https://cdn1.iconfinder.com/data/icons/ninja-things-1/1772/ninja-simple-512.png" alt="" /></div>Stranger Things&nbsp;S.1E.8 &nbsp;Rating: 5</a></li>
+                                        <li><a href="/profile"><div className="avatar"><img className="little_avatar" src="https://cdn1.iconfinder.com/data/icons/ninja-things-1/1772/ninja-simple-512.png" alt="" /></div>Game of Thrones&nbsp;Rating: 1</a></li>
                                     </ul>
                                 </div>
                             </div>
@@ -71,10 +71,10 @@ const Profile = props => (
                                         <input type="password" name="profile_password" disabled id="profile_password" className="form-control" value="password" />
                                     </div>
                                     <div className="form-group">
-                                        <button type="submit" className="btn btn-primary btn-radius">Save Changes</button>
+                                        {/* <button type="submit" className="btn btn-primary btn-radius">Save Changes</button> */}
                                     </div>
                                 </form>
-                            </div>
+                            {/* </div> */}
                         </div>
                     </div>
                 </div>
